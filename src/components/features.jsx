b52f85999206d04b55c36292bef5879b008ba75e
Saptarshi <bhattacharyasaptarshi2001@gmail.com>
2023-01-04 21:03:45 +0530

    fix

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -1,5 +1,5 @@
 export const Features = (props) => {
   return (
-    <iframe width="100%" height="100%" id="subscribe" title="form" src="https://6898db9c.sibforms.com/serve/MUIEAFcCOPT120Ak1mSEhpwHaZOYRREt59pw31Q95oWcbMgNvuzMIeaEWyco4kLMPIL6wc_0CSbOMdI_7GOwYBBIbDjdg4i_2J8Hzc6ETcq7gFBtnSBVg8kPOuRi5JwSo6pwtCKmK5tfi4yRk4RKzK0duArE4bmz4YvDCa1gL_yeAGVNwfti0b9o-stH62VKi2JVyqZeueQm-056"  style={{ display: "block", marginLeft: "auto", marginRight: "auto", maxWidth: "100%", backgroundColor: "#F6F6F6", }}></iframe>
+    <iframe width="100%" height="100%" id="subscribe" title="form" src="https://6898db9c.sibforms.com/serve/MUIEAIHMgCe9xJE4TLVhzF4iixuHm-Yz0I4uomQROPkuN9opU5fSrmeuTIfDQcCKmphMGOqNIjkKVpw01Sna6A5kn8T7mxx56MNmofBA8yzVpKcsuUk9XM-sjAKqGEpm5xr5YYsOVGeTNbwEiwL57gHcGQRhbVGG773HWCzN3VYz0UL65_vf1PSKtWYPSzZrQ48hZx6Y2k1W0jW8"  style={{ display: "block", marginLeft: "auto", marginRight: "auto", maxWidth: "100%", backgroundColor: "#F6F6F6", }}></iframe>
   )
 }
